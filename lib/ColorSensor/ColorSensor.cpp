48736eed9563e6f45232bdc7ca838a4c46404c18
Sabaniki <sawada13@mccc.jp>
2019-10-28 01:53:22 +0900

    引数に配列をとっても大丈夫なコンストラクタを定義、実装した

--- lib/ColorSensor/ColorSensor.cpp
+++ lib/ColorSensor/ColorSensor.cpp
@@ -1,6 +1,10 @@
 #include "ColorSensor.h"
 #include "Arduino.h"
 
-ColorSensor::ColorSensor() {
-
+ColorSensor::ColorSensor(int Rpin, int Gpin, int Bpin, const int (&thresholdsRGB)[3][3]): 
+light(Rpin, Gpin, Bpin) {
+    for (size_t i = 0; i < 3; i++)
+        for (size_t j = 0; j < 3; j++)
+            thresholds[i][j] = thresholdsRGB[i][j];
+        
 }
--- lib/ColorSensor/ColorSensor.h
+++ lib/ColorSensor/ColorSensor.h
@@ -1,10 +1,21 @@
 #ifndef ___Class_ColorSensor
 #define ___Class_ColorSensor
+#include "ColorLED.h"
 class ColorSensor {
 private:
+    int thresholds[3][3];
+    ColorLED light;
+    int aveR, aveG, aveB;
+	const int colorNum = 3;
+	const int loopNum = 10;
+	int mined[3][3];
+	int colorArray[3];
+	int readValue[3];
+	int copyedColorArray[3];
+	const char G = 'G', W = 'W', Bl = 'B';
 
 public:
-    ColorSensor();
+    ColorSensor(int Rpin, int Gpin, int Bpin, const int (&thresholdsRGB)[3][3]);
 };
 
 #endif
