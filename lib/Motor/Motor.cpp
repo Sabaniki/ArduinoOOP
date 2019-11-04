6bc5f0f4b094da47c803c76c5cc68ab450fa8ac3
Sabaniki <sawada13@mccc.jp>
2019-11-04 16:00:57 +0900

    impl Motor.cpp

--- lib/Motor/Motor.cpp
+++ lib/Motor/Motor.cpp
@@ -3,15 +3,40 @@
 #include "Motor.h"
 #include "Arduino.h"
 
-Motor::Motor(int Fpin, int Bpin) {
-
-}
-
-void Motor::write(int speed) {
+Motor::Motor(int Fpin, int Bpin):
+front(Fpin),
+back(Bpin),
+mode(Brake) {
 
 }
 
 void Motor::stop() {
+    front.write(0);
+    back.write(0);
+    delayMicroseconds(100);
+}
 
+void Motor::write(int speed) {
+    speed = constrain(speed, -255, 255);
+    if(speed > 0){
+        if(mode != Front)
+            stop();
+        front.write(speed);
+        back.write(0);
+        mode = Front;
+    }
+    else if(!speed){
+        if(mode != Brake)
+            stop();
+        front.write(speed);
+        back.write(speed);
+        mode = Brake;
+    }
+    else {
+        if(mode != Back)
+            front.write(0);
+            back.write(-speed);
+        mode = Back;
+    }
 }
 #endif
--- lib/Motor/Motor.h
+++ lib/Motor/Motor.h
@@ -1,11 +1,13 @@
 #ifndef ___Class_Motor
 #define ___Class_Motor
-
+#include "AnalogPin.h"
+#include "AnalogPin.cpp"
 class Motor {
 private:
-    int Fpin, Bpin, mode;
     void stop();
-
+    enum MotorMode { Front, Back, Brake };
+    AnalogPin front, back;
+    MotorMode mode;
 public:
     Motor(int Fpin, int Bpin);
     void write(int speed);
