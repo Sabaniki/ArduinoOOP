71238f497a1d64b163d892569a71b2565f977214
Sabaniki <sawada13@mccc.jp>
2019-11-04 16:17:30 +0900

    add RotaryEncoder

---
+++ lib/RotaryEncoder/RotaryEncoder.cpp
@@ -0,0 +1,9 @@
+#ifndef ___Cpp_RotaryEncoder
+#define ___Cpp_RotaryEncoder
+#include "RotaryEncoder.h"
+#include "Arduino.h"
+
+RotaryEncoder::RotaryEncoder() {
+
+}
+#endif
---
+++ lib/RotaryEncoder/RotaryEncoder.h
@@ -0,0 +1,10 @@
+#ifndef ___Class_RotaryEncoder
+#define ___Class_RotaryEncoder
+class RotaryEncoder {
+private:
+
+public:
+    RotaryEncoder();
+};
+
+#endif
