ef607ba5cc48242cf127c10b3538ee6748c521a9
Sabaniki <sawada13@mccc.jp>
2019-11-06 14:32:08 +0900

    考えていたアルゴリズムの一つ前のものを実装していた。あほくさ

--- lib/KuromikaLine/KuromikaLine.cpp
+++ lib/KuromikaLine/KuromikaLine.cpp
@@ -3,12 +3,22 @@
 #include "KuromikaLine.h"
 #include "Arduino.h"
 
-KuromikaLine::KuromikaLine(int Rpin, int Lpin, const int (&readerPins)[4], const int (&thresholdsRG)[2]) {
+KuromikaLine::KuromikaLine(int greenPin, const int (&readerPins)[4], int threshold):
+    threshold(threshold),
+    green(greenPin, OUTPUT) {
     for (size_t i = 0; i < 4; i++)
         readers[i].initPin(readerPins[i]);
+    green.write(true);
 }
 
 bool KuromikaLine::read(){
-    
+    beforeValue = readers[0].read();
+    nextValue = readers[0].read();
+    diff = beforeValue - nextValue;
+    bool isOverRange = abs(diff) > threshold;
+    if(isOverRange)
+        result = !result;
+    beforeValue = nextValue;
+    return result;
 }
 #endif
--- lib/KuromikaLine/KuromikaLine.h
+++ lib/KuromikaLine/KuromikaLine.h
@@ -2,6 +2,8 @@
 #define ___Class_KuromikaLine
 #include "AnalogPin.h"
 #include "AnalogPin.cpp"
+#include "DigitalPin.h"
+#include "DigitalPin.cpp"
 class KuromikaLine {
 private:
     AnalogPin readers[4] = { 
@@ -10,12 +12,20 @@ private:
         AnalogPin(),
         AnalogPin(),
     };
+    
+    DigitalPin green;
+    bool result = false;
+    int threshold;
+    int beforeValue = 0;
+    int nextValue = 0;
+    int diff = 0;
+
 
 public:
     // readerPins: { pin1: int, ... pin4: int } ← 本当はpin5まで
-    KuromikaLine(int Rpin, int Lpin, const int (&reaerPins)[4], const int (&thresholdsRG)[2]);
-    
-    // 黒が帰ってくるかどうか
+    KuromikaLine(int greenPin, const int (&reaerPins)[4],  int threshold);
+
+    // 黒が返ってくるかどうか
     bool read();
 };
 
