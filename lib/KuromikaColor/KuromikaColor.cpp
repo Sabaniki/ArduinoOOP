5793ef58f3065739840ec46f9a4b5b7feb892fdc
Sabaniki <sawada13@mccc.jp>
2019-11-07 11:59:58 +0900

    add KuromiColor

---
+++ lib/KuromikaColor/KuromikaColor.cpp
@@ -0,0 +1,9 @@
+#ifndef ___Cpp_KuromikaColor
+#define ___Cpp_KuromikaColor
+#include "KuromikaColor.h"
+#include "Arduino.h"
+
+KuromikaColor::KuromikaColor() {
+
+}
+#endif
---
+++ lib/KuromikaColor/KuromikaColor.h
@@ -0,0 +1,10 @@
+#ifndef ___Class_KuromikaColor
+#define ___Class_KuromikaColor
+class KuromikaColor {
+private:
+
+public:
+    KuromikaColor();
+};
+
+#endif
