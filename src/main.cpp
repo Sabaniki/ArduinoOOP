f1140c0786aced9d87d91cf78d91f86bd298bdc5
Sabaniki <sawada13@mccc.jp>
2019-11-22 10:10:45 +0900

    両方を一気にテストすることができる

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,41 @@ const int thresholdsOfRGB[3][3] = {
 };
 
 void loop() {
-    auto colorSensor = ColorSensor(10, 9, 8, A0, thresholdsOfRGB);
+    auto colorSensorL = ColorSensor(8, 14, 15, A8, thresholdsOfRGB);
+    auto colorSensorR = ColorSensor(8, 14, 15, A9, thresholdsOfRGB);
     while (true){
-        Serial.println(colorSensor.read());
         delay(100);
-        Serial.print("RedValue: ");
-        Serial.println(colorSensor.readValueRed());
-        delay(100);
-
-        Serial.print("GreenValue: ");
-        Serial.println(colorSensor.readValueGreen());
+        Serial.print("RedValue L: ");
+        Serial.println(colorSensorL.readValueRed());
         delay(100);
 
-        Serial.print("BlueValue: ");
-        Serial.println(colorSensor.readValueBlue());
+        Serial.print("GreenValue L: ");
+        Serial.println(colorSensorL.readValueGreen());
         delay(100);
+
+        Serial.print("BlueValue L: ");
+        Serial.println(colorSensorL.readValueBlue());
+        delay(100);
+        Serial.println();
+        Serial.println();
+
+        delay(100);
+        Serial.print("RedValue R: ");
+        
+        Serial.println(colorSensorR.readValueRed());
+        delay(100);
+
+        Serial.print("GreenValue R: ");
+        Serial.println(colorSensorR.readValueGreen());
+        delay(100);
+
+        Serial.print("BlueValue R: ");
+        Serial.println(colorSensorR.readValueBlue());
+        delay(100);
+
+        Serial.println();
+        Serial.print(colorSensorL.read());
+        Serial.println(colorSensorR.read());
         Serial.println();
     }
 }
