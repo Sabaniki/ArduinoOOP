ff844be1e5a34fab713e5003ace9176ce629e0b6
Sabaniki <sawada13@mccc.jp>
2020-02-10 15:26:58 +0900

    超音波センサのテストを編集

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
 #define DEBUG
-#include "Timer/Timer.h"
-#include "Arduino.h"
+#include <Arduino.h>
+#include "UltrasonicSensor/UltrasonicSensor.h"
+#include "DigitalPin/DigitalPin.h"
+
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    auto timer1 = Timer(5000, false, [](){
-        Serial.println("on timer1's non-named callback!");
-    });
-    auto timer2 = Timer(7000, false, [](){
-        Serial.println("on timer2's non-named callback!");
-    });
-    timer1.start();
-    timer2.start();
-    while (timer1.getIsUnreached() || timer2.getIsUnreached()) {
-        timer1.update();
-        timer2.update();
+    auto vcc = DigitalPin(22, OUTPUT);
+    vcc.write(HIGH);
+    delay(50);
+    auto uccSensor = UltrasonicSensor(10, 11);
+    while (true){
+        Serial.print(uccSensor.readDistance());
+        Serial.println(" cm");
     }
-    while (true);
 }
